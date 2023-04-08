709a84f827e118ff1fb7f5a3665a9f5244d7fc14
zhansheng <17600570787@163.com>
2023-04-09 00:08:47 +0800

    接口自动化

--- debugtalk.py
+++ debugtalk.py
@@ -1,4 +1,10 @@
-
+import os  
+							
+import sys
+							
+root_path=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  #获取当前工作路径、保存路径、获取当前路径的几种方法
+							
+sys.path.append(root_path)  #把获取到的工作路径添加到 系统路径下 。
 import time
 
 import os
